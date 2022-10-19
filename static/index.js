a8aeb923554856a01f330ad4236021214e8f1d6f
mattespinos <matthewwespinos@lewisu.edu>
2022-10-19 18:23:46 -0500

    updated index.js for client

--- static/index.js
+++ static/index.js
@@ -1,1 +1,28 @@
-// in case we need this
+async function calculateBMI(){
+    //Gets variables from the form
+    var height = document.getElementById("height");
+    height = height.toString();
+    var heightUnits = document.getElementById("heightUnits");
+    var weight = document.getElementById("weight");
+    weight = weight.toString();
+    var weightUnits = document.getElementById("weightUnits");
+    var age = document.getElementById("age");
+    age = age.toString();
+    var bloodPressure = document.getElementById("bloodPressure");
+
+    // See if Check Boxes are Checked
+    var diabetesCheck = document.getElementById("diabetesCheck");
+    const isDiabetesChecked = document.querySelector('#diabetesCheck');
+
+    var alzheimerCheck = document.getElementById("alzheimerCheck");
+    const isAlzheimerChecked = document.querySelector('#alzheimerCheck');
+
+    var cancerCheck = document.getElementById("cancerCheck");
+    const isCancerChecked = document.querySelector('#cancerCheck');
+
+    //response
+    const response = await fetch(`https://health-insurance-risk-calc-api.azurewebsites.net/api/bmi?height=${height}&heightUnits=${heightUnits}&weight=${weight}&weightUnits=${weightUnits}&age=${age}&bloodpressure=${bloodPressure}&diabetes=${isDiabetesChecked.checked}&alzheimers=${isAlzheimerChecked.checked}&cancer=${isCancerChecked.checked}`);
+    const responseJson = await response.json();
+    console.log(responseJson.bmi);
+    
+}
